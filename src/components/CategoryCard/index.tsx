366dedcf43a9563a23be69803aae69d22191e08e
Hootan Abasi Rocky <mast9rmind@gmail.com>
2022-08-05 01:24:45 +0430

    assets modified

---
+++ src/components/CategoryCard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CategoryCard() {
+    return (
+        <div></div>
+    );
+}
+
+export default CategoryCard;
---
+++ src/components/MostPopular/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MostPopular() {
+    return (
+        <div></div>
+    );
+}
+
+export default MostPopular;
