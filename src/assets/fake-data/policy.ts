761cd927781b75cd8b8d09c1ff07fcedbfd15223
Hootan <mast9rmind@gmail.com>
2022-08-15 00:56:43 +0430

    Change fake-data for policy cards

--- src/assets/fake-data/policy.ts
+++ src/assets/fake-data/policy.ts
@@ -1,19 +1,19 @@
 const policy = [
     {
-        name: "abc",
-        description: "It is fine",
+        name: "کالای با کیفیت",
+        description: "خرید کالای با کیفیت از معتبر ترین فروشنده های تایید شده",
         icon: "bx bx-shopping-bag"
     }, {
-        name: "abc",
-        description: "It is fine",
+        name: "پرداخت نقدی و قسطی",
+        description: "فراهم کردن انواع روش پرداخت برای راحتی کار مشتریان",
         icon: "bx bx-credit-card"
     }, {
-        name: "abc",
-        description: "It is fine",
+        name: "اعتماد",
+        description: "دو دهه فعالیت در زمینه ی فروش موبایل و لوازم جانبی افتخار ماست",
         icon: "bx bx-diamond"
     }, {
-        name: "abc",
-        description: "It is fine",
+        name: "محبت",
+        description: "درصدی از سود ماهیانه ی فیجی مستقیما به امور خیریه اختصاص دارد",
         icon: "bx bx-donate-heart"
     },
 ]
