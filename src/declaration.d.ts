8fc7109dee27fdd1e856b696f53ecad56cac0c68
Hootan <mast9rmind@gmail.com>
2022-08-19 10:45:29 +0430

    Add typescript module declaration

---
+++ src/declaration.d.ts
@@ -0,0 +1,13 @@
+declare module '*.scss';
+declare module "*.png" {
+    const value: any;
+    export default value;
+}
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+}
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
