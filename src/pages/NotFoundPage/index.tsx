c1f7e8f404c404419f5c62200f9f220c7a7da89c
Hootan <mast9rmind@gmail.com>
2022-09-01 15:32:56 +0430

    Add lottie animation for not found page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {Controls, Player} from "@lottiefiles/react-lottie-player";
 export const NotFoundPage = () => {
     return (
         <main className={'main'}>
-            <section>
-                <h1 >404</h1>
-                <h3 >متاسفانه صفحه ی مورد نظر پیدا نشد</h3>
+            <section className={'not-found'}>
+                <h3>متاسفانه صفحه ی مورد نظر پیدا نشد</h3>
+                <Player
+                    className={'not-found__player'}
+                    autoplay
+                    loop
+                    src="https://assets3.lottiefiles.com/packages/lf20_cr9slsdh.json"
+                >
+                    <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']}/>
+                </Player>
                 <Link to='/' className='btn'>
                     برگرد به صفحه ی اصلی
                 </Link>
