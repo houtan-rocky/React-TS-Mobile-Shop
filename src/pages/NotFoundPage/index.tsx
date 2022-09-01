12143109651b9ed271a0f8fdf206e570d4045fa9
Hootan <mast9rmind@gmail.com>
2022-09-01 15:39:32 +0430

    add typing animation package and use it in not found page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {Controls, Player} from "@lottiefiles/react-lottie-player";
+import {TypeAnimation} from 'react-type-animation';
+
 export const NotFoundPage = () => {
     return (
         <main className={'main'}>
             <section className={'not-found'}>
-                <h3>متاسفانه صفحه ی مورد نظر پیدا نشد</h3>
+
+
+                <TypeAnimation
+                    sequence={[
+                        'صفحه', // Types 'صفحه'
+                        1000, // Waits 1s
+                        'صفحه یافت', // Deletes 'One' and types 'Two'
+                        2000, // Waits 2s
+                        'صفحه یافت نشد', // Types 'Three' without deleting 'Two'
+                        2000, // Waits 2s
+
+                        () => {
+                            console.log('Done typing!'); // Place optional callbacks anywhere in the array
+                        }
+                    ]}
+                    wrapper="div"
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{fontSize: '2em'}}
+                />
+
                 <Player
                     className={'not-found__player'}
                     autoplay
