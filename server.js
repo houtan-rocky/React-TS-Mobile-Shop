051c2d1f31da81522cf0a4d4c8a469f7e21cc2e0
Hootan <mast9rmind@gmail.com>
2022-09-04 04:19:03 +0430

    Get server.js PORT dynamically from the environment

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const PORT = process.env.port || 3000;
+
 require('dotenv').config();
 const jsonServer = require('json-server');
 const cors = require('cors');
@@ -177,9 +179,8 @@ server.post([
 // Use default router (CRUDs of db.json)
 server.use(router);
 
-server.listen(process.env.port || 3000, () => {
-  const port = process.env.port || 3000;
-  console.log(`Customized JSON-Server is running at http://localhost:/${port}`);
+server.listen(PORT , () => {
+  console.log(`Customized JSON-Server is running at http://localhost:/${PORT}`);
 });
 
 module.exports = server;
